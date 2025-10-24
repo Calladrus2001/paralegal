a1b4de6b1a682d537d1aa54a3f2ac69e52ad28bf
Calladrus2001 <dugarvishesh@gmail.com>
2025-10-24 16:37:00 +0530

    add pdf chunks to weaviate

--- src/lambdas/processFileLambda/dev.ts
+++ src/lambdas/processFileLambda/dev.ts
@@ -1,15 +1,20 @@
 import { s3 } from "../../clients/aws";
 import { testS3EventRecord, testSQSS3Event } from "../../testfactories/aws";
 import { handler } from "./index";
+import fs from "fs/promises";
+import path from "path";
 
 const Bucket = process.env.S3_BUCKET_NAME;
 const Key = `CTM-1/CD-1`;
 
 (async () => {
+  const pdfPath = path.join(__dirname, "./sample_pdfs/sample_200KB.pdf");
+  const pdfBuffer = await fs.readFile(pdfPath);
   await handler(testSQSS3Event({ Records: [testS3EventRecord({ Bucket, Key })] }));
   await s3.putObject({
     Bucket,
     Key,
+    Body: pdfBuffer,
   });
   process.exit(0);
 })().catch((error) => {
--- src/lambdas/processFileLambda/index.ts
+++ src/lambdas/processFileLambda/index.ts
@@ -1,5 +1,45 @@
-import type { SQSEvent } from "aws-lambda";
+import type { S3Event, SQSEvent } from "aws-lambda";
+import { s3 } from "../../clients/aws";
+import weaviateClient from "../../clients/weaviate";
+import { PDFParse } from "pdf-parse";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-export const handler = async (event: SQSEvent) => { 
-  console.log(event)
-}
+export const handler = async (event: SQSEvent) => {
+  try {
+    const s3Event: S3Event = JSON.parse(event.Records[0]!.body);
+    const s3Record = s3Event.Records[0]!.s3;
+    const Bucket = s3Record.bucket.name;
+    const Key = s3Record.object.key;
+
+    const command = new GetObjectCommand({ Bucket, Key });
+    const presignedUrl = await getSignedUrl(s3, command, { expiresIn: 900 });
+    const res = await fetch(presignedUrl);
+    const arrayBuffer = await res.arrayBuffer();
+
+    const parser = new PDFParse({ data: arrayBuffer });
+    const result = await parser.getText();
+    await parser.destroy();
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 100,
+    });
+    const docs = await splitter.createDocuments([result.text]);
+    const paralegal = weaviateClient.collections.use("paralegal");
+    await Promise.all(
+      docs.map((doc, i) =>
+        paralegal.data.insert({
+          properties: {
+            text: doc.pageContent,
+            chunk_index: i,
+            source: Key,
+          },
+        })
+      )
+    );
+  } catch (error: any) {
+    console.error(error);
+  }
+};
