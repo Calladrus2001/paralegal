683d4d35f8f774ec1084451db9a958bc20025687
Calladrus2001 <dugarvishesh@gmail.com>
2026-08-20 12:08:07 +0530

    configure tfstate s3 remote backend

--- infra/host/provider.tf
+++ infra/host/provider.tf
@@ -4,6 +4,12 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+
+  backend "s3" {
+    bucket = "paralegal-tf-state"
+    key    = "host/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
